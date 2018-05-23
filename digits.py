78d3adb1a31503da4e3f8dba061af9b443c40cd2
Eduardo Zimermam <eduardozimerman@live.com>
2018-05-23 18:09:03 -0300

    -Adição dos arquivos para leitura dos digitos e os digitos em si.

---
+++ digits.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+# Método que lê um arquivo e retorna uma lista contendo cada linha do arquivo
+def leArquivo(nomeArquivo):
+	listalinhas = open(nomeArquivo, 'r').read().splitlines()
+
+	return(listalinhas)
+
+def dissimPorHistograma(imagem):
+	img = cv2.imread(imagem, 0)
+
+	linhas, colunas = img.shape
+	
+	_, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+	
+	if(linhas < colunas):
+		tamHistogramas = linhas
+	else:
+		tamHistogramas = colunas
+	
+	histHorizontal = [0] * tamHistogramas
+	histVertical = [0] * tamHistogramas
+	
+	histAcumulHorizontal = [0] * tamHistogramas
+	histAcumulVertical = [0] * tamHistogramas
+	
+	dissimilaridade = 0
+	
+	
+	for i in range(0, tamHistogramas):
+		for j in range(0, tamHistogramas):
+			if(thresh[i][j] == 0):
+				histVertical[i] += 1
+	
+	for j in range(0, tamHistogramas):
+		for i in range(0, tamHistogramas):
+			if(thresh[i][j] == 0):
+				histHorizontal[j] += 1
+	
+	histAcumulVertical[0] = histVertical[0]
+	
+	for i in range(1, tamHistogramas):
+		histAcumulVertical[i] = histAcumulVertical[i - 1] + histVertical[i]
+	
+	histAcumulHorizontal[0] = histHorizontal[0]
+	
+	for i in range(1, tamHistogramas):
+		histAcumulHorizontal[i] = histAcumulHorizontal[i - 1] + histHorizontal[i]
+	
+	for i in range(tamHistogramas):
+		dissimilaridade += abs(histAcumulVertical[i] - histAcumulHorizontal[i])
+	
+	return(dissimilaridade)
